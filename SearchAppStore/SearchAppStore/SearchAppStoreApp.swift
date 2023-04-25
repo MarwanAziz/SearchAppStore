b3a80505ae97976b0d402ceff436346c76c18858
Marwan Aziz <marwan@marwanaziz.net>
2023-04-25 14:27:17 +0100

    Initial Project creation.

---
+++ SearchAppStore/SearchAppStore/SearchAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppStoreApp.swift
+//  SearchAppStore
+//
+//  Created by Marwan Aziz on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
